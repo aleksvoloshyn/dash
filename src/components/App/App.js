250aece54831192040e027202272cffbe25e4831
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2022-06-13 12:56:04 +0300

    color and font styles added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import Container from '../Container/Container'
+import Navigation from '../Navigation'
 // import ExampleComponent from '../ExampleComponent'
+
 import styles from './app.module.scss'
 
 function App() {
   return (
     <Container>
       {/* <ExampleComponent numb={5} /> */}
-      <div className={styles.App}>my react app template</div>
+      {/* <div className={styles.App}>my react app template</div> */}
+      <Navigation />
     </Container>
   )
 }
